51518243c2d5b458fcee0220d88c579f5e44eeb1
Taylor Spears <tcs@tcspears.net>
2015-06-16 22:46:24 +0100

    Added functionality to deal with multiple values; needed for stock prices

--- R/FundamentalsInfo.R
+++ R/FundamentalsInfo.R
@@ -14,11 +14,22 @@ FundamentalsInfo <- function(firm.name,fundamental.code,sheets){
   names(firm.sheets) <- sapply(firm.sheets,FUN=function(x) attributes(x)$type)
   
   # Determine which sheet contains the desired fundamental.
-  matching.location <- sapply(firm.sheets,FUN=function(x) which(x[,1]==fundamental.code))
+  matching.location <- lapply(firm.sheets,FUN=function(x) which(x[,1]==fundamental.code))
   
   # Throw an error if there are no matching sheets/locations
-  if(length(unlist(matching.location))==0){
+  if(length(unlist(matching.location)) == 0){
     stop("No such fundamental exists within the sheets")
+  }
+  
+  # If there are multiple matching entries, then see if the name is the same.
+  if(length(unlist(matching.location)) > 1){
+    matches <- mapply(firm.sheets,matching.location,FUN=function(x,y) x[y,2])
+    matches <- unique(matches[!is.na(matches)])
+    if(length(unlist(matches)) == 1){
+      return(c("Multiple","Multiple",matches))
+    } else {
+      stop("Error: Multiple matching entries with different values")
+    }
   }
   
   # Determine the location of the fundamental within the sheet and its full name.
